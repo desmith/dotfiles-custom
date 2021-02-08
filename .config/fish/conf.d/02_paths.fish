874af4eabafb786a626f000e1e1509dd032b9042
Dave E Smith <desmith@gmail.com>
2021-02-08 18:10:51 -0500

    updates

--- .config/fish/conf.d/02_paths.fish
+++ .config/fish/conf.d/02_paths.fish
@@ -1,10 +1,10 @@
 set -gx PROJECT_PATHS ~/code ~/projects /var/www
 
-for path in ~/.local/bin ~/bin ~/bin/certs ~/bin/mysql ~/bin/perl ~/bin/pgres ~/bin/php ~/bin/py ~/bin/ruby ~/gocode/bin /usr/local/mysql/bin /usr/local/bin ;
+for path in ~/.local/bin ~/bin ~/bin/certs ~/bin/mysql ~/bin/perl ~/bin/pgres ~/bin/php ~/bin/py ~/bin/ruby ~/gocode/bin /usr/local/mysql/bin /usr/local/bin /opt/homebrew/bin;
 
     #echo "added $path to PATH..."
     set -agx PATH $path;
 
 end
 
-#set -agx PATH /usr/local/opt/python/libexec/bin:$PATH
+set -agx PATH /opt/homebrew/bin:$PATH
