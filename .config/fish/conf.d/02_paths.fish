bb42f066fd0cc15c9d9511a224f556cf55883652
Dave E Smith <desmith@gmail.com>
2021-02-08 18:38:03 -0500

    fixed path setting...

--- .config/fish/conf.d/02_paths.fish
+++ .config/fish/conf.d/02_paths.fish
@@ -2,6 +2,8 @@ set -gx PROJECT_PATHS ~/code ~/projects /var/www
 
 for newpath in /opt/homebrew/bin ~/.local/bin ~/bin ~/bin/certs ~/bin/mysql ~/bin/perl ~/bin/pgres ~/bin/php ~/bin/py ~/bin/ruby ~/gocode/bin /usr/local/mysql/bin /usr/local/bin ;
 
-    set -agx PATH $newpath;
+    contains -- $newpath $fish_user_paths
+       or set -U fish_user_paths $fish_user_paths $newpath
+    #set -agx PATH $newpath;
 
 end
